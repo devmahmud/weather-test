c0c66d05f442463abe2442f435ca73229c3f435b
Mahmudul Alam <expelmahmud@gmail.com>
2021-04-14 12:11:55 +0600

    first commit

--- Home/Home/settings.py
+++ Home/Home/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'Home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
