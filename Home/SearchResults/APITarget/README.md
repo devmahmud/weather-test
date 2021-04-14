bfb6b1dda53f36803e243511fd0910a943a09ee0
Mahmudul Alam <expelmahmud@gmail.com>
2021-04-14 12:52:15 +0600

    fix project structure

--- Home/SearchResults/APITarget/README.md
+++
@@ -1,1 +0,0 @@
-# Ibex_API-Feeds
--- Home/pages/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def openMainPage(request, *args, **kwargs):
-    return render(request,"TravellyHomePage.html",{})
