a94ab7917d3a592f2c54695834dd2c19f7279945
Mahmudul Alam <expelmahmud@gmail.com>
2021-04-14 14:01:37 +0600

    add: travelplan model test

--- SearchResults/models.py
+++ SearchResults/models.py
@@ -2,10 +2,14 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 # Create your models here.
+
+
 class TravelPlan(models.Model):
-    travelID     = models.PositiveIntegerField(unique=True, default=0, editable=False)
-    origin       = models.CharField(max_length=50)
-    destination  = models.CharField(max_length=50)
-    price        = models.DecimalField(max_digits=6, decimal_places=2)
-    travelOrigins =  ArrayField(models.CharField(max_length=100),default=list)
-    travelDestination =  ArrayField(models.CharField(max_length=100),default=list)
+    travelID = models.PositiveIntegerField(
+        unique=True, default=0, editable=False)
+    origin = models.CharField(max_length=50)
+    destination = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    travelOrigins = ArrayField(models.CharField(max_length=100), default=list)
+    travelDestination = ArrayField(
+        models.CharField(max_length=100), default=list)
--- SearchResults/tests.py
+++ SearchResults/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from SearchResults.models import TravelPlan
 
-# Create your tests here.
+
+class SearchResultsTests(TestCase):
+    """
+    Test class for Search Results
+    """
+
+    # def test_travelplan_display(self):
+    #     """
+    #     Test travelpan display result
+    #     """
+    #     response = self.client.get(
+    #         "/resultspage/?origin=DHAKA&destination=USA&travelDate=2020-10-04")
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTemplateUsed(response, 'SearchResults.html')
+
+    def test_travelplan_model(self):
+        """
+        Test TravelPlan model
+        """
+        obj = TravelPlan.objects.create(
+            origin="CANADA", destination="AUSTRALIA", price=200, travelOrigins=["orgin1", "origin2"],
+            travelDestination=['destination1', 'destination2']
+        )
+        self.assertEqual(obj.id, 1)
+        self.assertEqual(obj.origin, 'CANADA')
+        self.assertEqual(obj.destination, 'AUSTRALIA')
+        self.assertEqual(obj.price, 200)
+        self.assertEqual(obj.travelOrigins, ["orgin1", "origin2"])
+        self.assertEqual(obj.travelDestination, [
+                         'destination1', 'destination2'])
